7c09cf79232df190127c3b2375bf3c81b358f837
Wonbin, Bae <wonbinb@uci.edu>
2026-08-05 15:06:33 -0700

    Finish all the basic-fundamental examples of ML

---
+++ deep_neural_network.py
@@ -0,0 +1,61 @@
+from tensorflow import keras
+(train_input, train_target), (test_input, test_target) =\
+    keras.datasets.fashion_mnist.load_data()
+
+from sklearn.model_selection import train_test_split
+train_scaled = train_input / 255.0
+train_scaled = train_scaled.reshape(-1, 28*28)
+train_scaled, val_scaled, train_target, val_target = train_test_split(train_scaled, train_target, test_size=0.2, random_state=42)
+
+## Hidden Layers
+dense1 = keras.layers.Dense(100, activation='sigmoid', input_shape=(784,))
+dense2 = keras.layers.Dense(10, activation='softmax')
+
+# Deep neural network(DNN)
+model = keras.Sequential([dense1, dense2])
+model.summary()
+
+model = keras.Sequential([
+    keras.layers.Dense(100, activation='sigmoid', input_shape=(784,), name='hidden'),
+    keras.layers.Dense(10, activation='softmax', name='output')
+], name='Fashion_MNIST_model')
+
+model.summary()
+
+model = keras.Sequential()
+model.add(keras.layers.Dense(100, activation='sigmoid', input_shape=(784,)))
+model.add(keras.layers.Dense(10, activation='softmax'))
+model.summary()
+
+model.compile(loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.fit(train_scaled, train_target, epochs=5)
+
+model = keras.Sequential()
+model.add(keras.layers.Flatten(input_shape=(28,28)))
+model.add(keras.layers.Dense(100, activation='relu'))
+model.add(keras.layers.Dense(10, activation='softmax'))
+model.summary()
+
+(train_input, train_target), (test_input, test_target) =\
+    keras.datasets.fashion_mnist.load_data()
+train_scaled = train_input / 255.0
+train_scaled, val_scaled, train_target, val_target = train_test_split(train_scaled, train_target, test_size=0.2, random_state=42)
+
+model.compile(loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.fit(train_scaled, train_target, epochs=5)
+model.evaluate(val_scaled, val_target)
+
+# Optimizer
+## Momentum Optimization
+## Nesterov Momentum Optimization
+## Adaptive Learning Rate
+
+model = keras.Sequential()
+model.add(keras.layers.Flatten(input_shape=(28, 28)))
+model.add(keras.layers.Dense(100, activation='relu'))
+model.add(keras.layers.Dense(10, activation='softmax'))
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.fit(train_scaled, train_target, epochs=5)
+
+model.evaluate(val_scaled, val_target)
